faccd61d125c3700c4b7d2dfe4146059f3911e1a
ellen <earteca@laurentian.ca>
2016-03-01 15:05:17 -0500

    wow

--- runner.sh
+++ runner.sh
@@ -7,9 +7,15 @@ exNum=`echo 1`
 
 while (( exNum <= 10 )); do
 	theMethod=1
+	algoType=1
 	while (( theMethod <= 3 )); do
-		sed s/"exNum := 1"/"exNum := `echo $exNum`"/ timer.mpl | sed s/"theMethod := 1"/"theMethod := `echo $theMethod`"/ ~/maple13/bin/maple
-		(( theMethod = $theMethod + 1 ))
+		if (( algoType < 2 )); then
+			sed s/"exNum := 1"/"exNum := `echo $exNum`"/ timer.mpl | sed s/"theMethod := 1"/"theMethod := `echo $theMethod`"/ | sed s/"theAlgoType := buchberger"/"theAlgoType := maplef4"/ | ~/maple13/bin/maple
+			(( algoType = $algoType + 1 ))
+		else
+			sed s/"exNum := 1"/"exNum := `echo $exNum`"/ timer.mpl | sed s/"theMethod := 1"/"theMethod := `echo $theMethod`"/ | ~/maple13/bin/maple
+			(( theMethod = $theMethod + 1 ))
+		fi
 	done
 	(( exNum = $exNum + 1 ))
 done
